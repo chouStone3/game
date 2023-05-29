e3760d92b56385dbe11d46fea6cefe102427f8a6
chouStone3 <415299654@qq.com>
2023-05-29 13:11:34 +0800

    reducer

--- src/Prague-reducer/index.tsx
+++ src/Prague-reducer/index.tsx
@@ -1,78 +1,99 @@
-import { useState } from "react"
+import { useReducer, useState } from "react"
 import styled from "styled-components";
+interface Task {
+    id: number,
+    text: string,
+    done: boolean
+}
 
+let nextId = 3;
 export default function PragueReducer() {
-    const initList = [{ id: 0, text: '参观卡夫卡博物馆', done: true },
+    const initList: Task[] = [{ id: 0, text: '参观卡夫卡博物馆', done: true },
     { id: 1, text: '看木偶戏', done: false },
     { id: 2, text: '打卡列侬墙', done: false },]
-    const [taskList, setTL] = useState(initList)
 
-    const handleAddTask = (newTask: { id: number; text: string; done: boolean }) => {
-        setTL([...taskList, newTask])
+    const [tasks, disPatch] = useReducer(tasksReducter, initList)
+
+    const handleAddTask = (text: string) => {
+        disPatch({ id: nextId++, type: 'added', text })
     }
 
     const handleDeletTask = (taskId: number) => {
-        setTL(taskList.filter((t) => t.id !== taskId))
-
+        disPatch({ id: taskId, type: 'deleted' })
     }
 
-    const handleEditTask = (taskId: number, newText: string) => {
-        setTL(taskList.map((tas) => {
-            return tas.id === taskId ? { ...tas, text: newText } : tas
-        }))
+    const handleChangeTask = (task: Task) => {
+        disPatch({ ...task, type: 'changed' })
+    }
+
+    function tasksReducter(tasks: any, action: any) {
+        switch (action.type) {
+            case 'added': {
+                return [...tasks, { id: action.id, text: action.text, done: false }]
+            }
+            case 'changed': {
+                return tasks.map((task: Task) => {
+                    return task.id === action.id ? { ...task, text: action.text, done: action.done } : task
+                })
+            }
+            case 'deleted': {
+                return tasks.filter((task: Task) => task.id !== action.id)
+            }
+            default: {
+                throw Error('未知操作：' + action.type);
+            }
+        }
     }
 
     return <>
         <h1>布拉格行程(reducer)</h1>
-        <AddTask setTL={handleAddTask} />
-        <TaskList taskList={taskList} setTL={setTL} />
+        <AddTask
+            addTask={handleAddTask}
+        />
+        <TaskList
+            tasks={tasks}
+            onDeleteTask={handleDeletTask}
+            onChangeTask={handleChangeTask}
+        />
     </>
 }
 
-function AddTask({ setTL }: { setTL: (s: any) => void }) {
-    const [newTask, setNT] = useState('')
-    const [nextId, setNI] = useState(1)
+function AddTask({ addTask }: { addTask: (text: string) => void }) {
+    const [newText, setNT] = useState('')
     return <label>
-        <input value={newTask} onChange={(e) => setNT(e.target.value)}></input>
+        <input value={newText} onChange={(e) => setNT(e.target.value)}></input>
         <Button onClick={() => {
-            setTL({ id: nextId, text: newTask, done: false })
-            setNI(nextId + 1)
+            addTask(newText)
+            setNT('')
         }} >添加</Button>
     </label>
 }
 
-function TaskList({ taskList, setTL }: { taskList: any[], setTL: (s: any) => void }) {
-    console.log(taskList);
-
+function TaskList({ tasks, onDeleteTask, onChangeTask }:
+    { tasks: any[], onDeleteTask: (taskId: number) => void, onChangeTask: (task: Task) => void }) {
     return <Ul>
-        {taskList.map((task) => {
+        {tasks.map((task) => {
             return <Li key={task.id}>
-                <Task task={task} setList={setTL} taskList={taskList} />
+                <Task task={task} onDeleted={onDeleteTask} onChanged={onChangeTask} />
             </Li>
         })}
     </Ul>
 }
 
-const Task = ({ task, taskList, setList }: { task: any, taskList: any[], setList: (s: any) => void }) => {
+const Task = ({ task, onDeleted, onChanged }: { task: any, onDeleted: (taskId: number) => void, onChanged: (task: Task) => void }) => {
     const [isEditing, setEditing] = useState(false)
     return <label>
         <input type='checkbox' onChange={(e) => {
-            setList(taskList.map(item => {
-                return item.id === task.id ? { ...task, done: e.target.checked } : task
-            }))
+            onChanged({ ...task, done: e.target.checked })
         }} />
         {isEditing ? <>
-            <input value={task.text} onChange={(e) =>
-                setList(taskList.map((tas) => {
-                    return tas.id === task.id ? { ...tas, text: e.target.value } : tas
-                }))
-            } />
+            <input value={task.text} onChange={(e) => onChanged({ ...task, text: e.target.value })} />
             <Button onClick={() => setEditing(false)}>保存</Button>
         </> : <>
             {task.text}
             <Button onClick={() => setEditing(true)}>编辑</Button>
         </>}
-        <button onClick={() => setList(taskList.filter((t) => t.id !== task.id))}>删除</button>
+        <button onClick={() => onDeleted(task.id)}>删除</button>
     </label>
 
 }
